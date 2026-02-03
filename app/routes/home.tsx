ccde159c6584e88c95c03b2e62a0767af234a025
ehvzmf <yuna00708@gmail.com>
2026-02-03 22:58:03 +0900

    [refactor] Update home page to use Header and StockTable components

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Header } from "../components/Header";
+import { StockTable } from "../components/StockTable";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Otto's Lab - 주식 투자 분석" },
+    { name: "description", content: "Otto's Lab 주식 투자 포트폴리오 관리 시스템" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <Header />
+      <StockTable />
+    </div>
+  );
 }
